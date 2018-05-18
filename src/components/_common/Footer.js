c280c3692e29bc5d285859b2d6bf24b9bd131690
Ez-Z <377210988@qq.com>
2018-05-18 16:01:49 +0800

    pure-render

--- src/components/_common/Footer.js
+++ src/components/_common/Footer.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import pureRender from 'pure-render-decorator';
 
-const Header = (props) => {
-	return (
-		<div className="common-footer">
-			<h1>我是footer</h1>
-			
-		</div>
-	);
+@pureRender
+class Footer extends Component {
+	constructor(){
+        super();
+    }
+	
+    render(){
+    	
+    	return (
+			<div className="common-footer">
+				<h1>我是footer</h1>
+			</div>
+		);
+    }
+	
 };
-
-export default Header;
+export default Footer;
--- src/components/_common/Header.js
+++ src/components/_common/Header.js
@@ -1,31 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import './Header.scss';
 import pureRender from 'pure-render-decorator';
+@pureRender
+class Header extends Component {
+	constructor(){
+        super();
+    }
+	
+    render(){
+    	const {
+			name,
+		} = this.props;
+    	return (
+			<div className="common-header">
+				<h1>
+					<Link to="/">
+						logo返回首页
+					</Link>
+				</h1>
+				<div>
 
-const Header = (props) => {
-	const {
-		name,
-	} = props;
-
-	return (
-		<div className="common-header">
-			<h1>
-				<Link to="/">
-					logo返回首页
-				</Link>
-			</h1>
-			<div>
-
-				<span><Link to="/">Home</Link> </span>
-				<span><Link to="/test">test</Link> </span>
-				<span>
-					<span className="_name">我是{name} </span>
-				</span>
+					<span><Link to="/">Home</Link> </span>
+					<span><Link to="/test">test</Link> </span>
+					<span>
+						<span className="_name">我是{name} </span>
+					</span>
+				</div>
+				
 			</div>
-			
-		</div>
-	);
+		);
+    }
+	
 };
 
 export default Header;
