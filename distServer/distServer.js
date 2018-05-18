c5c1d239fdfdfe0b17929fff853112cef7b80c5c
Ez-Z <377210988@qq.com>
2018-05-18 18:06:19 +0800

    distserve

--- distServer/distServer.js
+++ distServer/distServer.js
@@ -4,7 +4,9 @@ let express = require('express');
 let app = express();
 //指定启动服务器到哪个文件夹，我这边指的是dist文件夹
 app.use(express.static('./dist'));
-
+app.get('/*', function() {
+	res.sendfile('dist/index.html')
+});
 // 监听端口为3000
 let server = app.listen(3000, function () {
     let port = server.address().port;
