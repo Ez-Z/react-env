aa41188c347acd625d8d840309b638f7007050f4
zhoushengda <377210988@qq.com>
2019-11-01 15:36:25 +0800

    update

---
+++ src/utils/dva.js
@@ -0,0 +1,35 @@
+import { create } from 'dva-core';
+import { createLogger } from 'redux-logger';
+import createLoading from 'dva-loading';
+ 
+let app;
+let store;
+let dispatch;
+ 
+function createApp(opt) {
+  // redux日志
+  if (process.env.NODE_ENV === 'development' && process.env.TARO_ENV !== 'quickapp') {
+    opt.onAction = [createLogger()];
+  }
+  app = create(opt);
+  app.use(createLoading({}));
+ 
+  if (!global.registered) opt.models.forEach(model => app.model(model));
+  global.registered = true;
+  app.start();
+ 
+  store = app._store;
+  app.getStore = () => store;
+ 
+  dispatch = store.dispatch;
+ 
+  app.dispatch = dispatch;
+  return app;
+}
+ 
+export default {
+  createApp,
+  getDispatch() {
+    return app.dispatch;
+  }
+}
